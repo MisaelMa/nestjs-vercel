ba4c5fb2a6c5df38468cbdf62e6b672c0597a92d
MisaelMa <amisael.amir.misael@gmail.com>
2024-11-20 12:27:49 -0500

    feat(nest): update methods

--- src/app.controller.ts
+++ src/app.controller.ts
@@ -1,4 +1,13 @@
-import { Controller, Get, Post, Body } from '@nestjs/common';
+import {
+  Controller,
+  Get,
+  Post,
+  Body,
+  Patch,
+  Param,
+  Put,
+  Delete,
+} from '@nestjs/common';
 import { AppService } from './app.service';
 
 @Controller()
@@ -16,6 +25,30 @@ export class AppController {
   saveInfo(@Body() body: any) {
     return {
       ...body,
+    };
+  }
+
+  @Patch(':id')
+  updateInfo(@Body() body: any, @Param('id') id: string) {
+    return {
+      id,
+      ...body,
+    };
+  }
+
+  @Put(':id')
+  replaceInfo(@Body() body: any, @Param('id') id: string) {
+    return {
+      id,
+      ...body,
+    };
+  }
+
+  @Delete(':id')
+  deleteInfo(@Param('id') id: string) {
+    return {
+      id,
+      message: 'Deleted',
     };
   }
 }

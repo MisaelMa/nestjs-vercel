cfbccac5fd2501b2d99065e15e89e742c945af1e
MisaelMa <amisael.amir.misael@gmail.com>
2024-11-30 05:50:05 -0500

    feat(core): added migrations

--- src/config/typeorm.config.ts
+++ src/config/typeorm.config.ts
@@ -1,5 +1,6 @@
 import { DataSource } from 'typeorm';
 import { postgresConfig } from './database.config';
+import { ConfigModule } from '@nestjs/config';
 
 ConfigModule.forRoot({
   envFilePath: `.env`,

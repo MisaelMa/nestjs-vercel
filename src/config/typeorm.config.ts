55ec6ddf8022b132378b52afc26ac04b3a6aa622
MisaelMa <amisael.amir.misael@gmail.com>
2024-11-30 05:41:45 -0500

    feat(core): added migrations

--- src/config/typeorm.config.ts
+++ src/config/typeorm.config.ts
@@ -1,6 +1,10 @@
 import { DataSource } from 'typeorm';
 import { postgresConfig } from './database.config';
 
+ConfigModule.forRoot({
+  envFilePath: `.env`,
+});
+
 const AppDataSource = new DataSource(postgresConfig());
 
 export default AppDataSource;
